5766d03066f4e67cb2bebee048cc5bcef5f830e7
Eraldo <eraldo@iMac-de-Webgoal-3.local>
2016-12-10 16:26:52 -0200

    Checkin backend

--- backend/app/controllers/checkin_controller.rb
+++ backend/app/controllers/checkin_controller.rb
@@ -1,12 +1,12 @@
 class CheckinController < ApplicationController
 
   def index
-    @usuarios = User.findAll()
+    @usuarios = Usuario.findAll()
     @atividades = Atividade.findAll()
   end
 
   def checkin(usuario,atividade)
-    checkin = Checkin.create(:usuario_id => usuario.id, :atividade => atividade.id)
+    checkin = Checkin.create(:usuario_id => usuario.id, :atividade_id => atividade.id)
     #redirect_to()
   end
 
---
+++ backend/app/controllers/painelmedalha_controller.rb
@@ -0,0 +1,4 @@
+class PainelmedalhaController < ApplicationController
+  def index
+  end
+end
