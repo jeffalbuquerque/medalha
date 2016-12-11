2267a8dde7a33f735bdb867263919fb713a3c545
Time #3 <curso@webgoal.com.br>
2016-12-11 10:55:20 -0200

    Criação de hash para uso na view do painel

--- backend/app/controllers/checkins_controller.rb
+++ backend/app/controllers/checkins_controller.rb
@@ -1,10 +1,15 @@
 class CheckinsController < ApplicationController
 
+  attr_accessor :checkins
+
+  checkins =  Hash.new {:usuario_id=>[1, 1, 3, 2, 3],
+      :atividade_id=>[1, 2, 3, 1, 2],
+      :quantidade=>[4, 11, 1, 5, 5],
+      :medalha=>["bronze", "ouro", "", "prata", "prata"]
+  }
+
   def index
-    @usuarios = Usuario.all
-    @atividades = Atividade.all
-    # @usuarios = Usuario.all
-    # @atividades = Atividade.all
+    checkins
   end
 
   def checkin(usuario,atividade)
